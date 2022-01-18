38260c2d71266bb577bc7acf86d1d34e8dd5b864
Azide <rukuy@qq.com>
2022-01-18 22:29:29 +0800

    初次尝试typecho提供的功能

---
+++ Kaltsit/header.php
@@ -0,0 +1,15 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="<?php $this->options->charset(); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="renderer" content="webkit">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+    <title>
+        <?php $this->options->title(); ?>
+    </title>
+    <link rel="stylesheet" href="<?php $this->options->themeUrl('kaltsit.css'); ?>">
+</head>
+
+<body>
